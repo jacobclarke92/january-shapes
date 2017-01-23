c312a6d508eb01bda71c068eccda212d32f79024
Jacob Clarke <jacob@bigfish.tv>
2017-01-23 10:28:25 +1000

    Did a thing :O

--- scripts/index.js
+++ scripts/index.js
@@ -10,14 +10,14 @@ import {
 
 // Init PIXI renderer
 const $app = $('#app');
-const renderer = PIXI.autoDetectRenderer(
+const renderer = autoDetectRenderer(
 	getScreenWidth()/getPixelDensity(), 
 	getScreenHeight()/getPixelDensity(), 
 	{
 		resolution: getPixelDensity(),
 		transparent: false,
 		backgroundColor: 0x333333,
-		antialiasing: true,
+		antialias: true,
 	}
 );
 const canvas = renderer.view;
@@ -26,11 +26,49 @@ $app.append(canvas);
 
 const stageWrapper = new Container();
 
+const radToDeg = rad => rad*180/Math.PI;
+const degToRad = deg => deg/180*Math.PI;
+const randomFloat = (min, max) => min + Math.random()*(max-min);
+const randomInt = (min, max) => min + Math.floor(Math.random()*(max-min));
+const getRandomValueFromArray = array => array[Math.floor(Math.random()*array.length)];
+
 function init() {
 	console.log('Hello world');
 	console.log($app);
 
-	stageWrapper.addChild(createTriangle());
+	let palette = getRandomValueFromArray(palettes);
+	const backgroundColor = getRandomValueFromArray(palette);
+	renderer.backgroundColor = backgroundColor;
+	palette = palette.filter(val => val != backgroundColor);
+
+	const rows = 50;
+	const cols = 36;
+	const paddingY = 200;
+
+	const rowHeight = (getScreenHeight() + paddingY*2) / rows;
+
+	const baseAngle = 20;
+
+	for(let n=0; n < rows; n ++) {
+
+		const startPos = {x: -20, y: -paddingY + rowHeight*n};
+		const angleRange = 20 + randomInt(-20, 20);
+		let theta = Math.random()*Math.PI;
+
+		for(let i=0; i<cols; i ++) {
+			theta += 0.5;
+			const color = getRandomValueFromArray(palette);
+			const angle = baseAngle + Math.cos(theta)*angleRange;
+			const distance = randomInt(10, 30);
+			const triangle = createTriangle({startPos, distance, angle, color});
+			triangle.scale.y = (Math.random() > 0.5 ? -1 : 1) * randomFloat(0.5, 1);
+			stageWrapper.addChild(triangle);
+
+			startPos.x = startPos.x + Math.cos(degToRad(angle))*distance;
+			startPos.y = startPos.y + Math.sin(degToRad(angle))*distance;
+		}
+
+	}
 
 	animate();
 }
@@ -40,7 +78,7 @@ function createTriangle(opts = {}) {
 	const { color = 0xCCCCCC, angle = 45, startPos = {x: 50, y: 50}, distance = 50 } = opts;
 
 	const triangle = new Graphics();
-	triangle.lineStyle(1, 0xFFFFFF, 1);
+	triangle.lineStyle(0, 0xFFFFFF, 1);
 	triangle.beginFill(color);
 	triangle.moveTo(0,0);
 	triangle.lineTo(distance/2, distance/2);
@@ -50,6 +88,7 @@ function createTriangle(opts = {}) {
 	triangle.position = startPos;
 	triangle.pivot.set(0,0);
 	triangle.rotation = angle/180*Math.PI;
+	triangle.cacheAsBitmap = true;
 	return triangle;
 }
 
